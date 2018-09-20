ec567bf686535894675f3fb28979031c65a3b190
#CAI LINGZHI# <LCAI004@student.main.ntu.edu.sg>
2018-09-20 17:10:56 +0800

    Update Lab 2

--- lab2/Lab2/src/test/ParserTests.java
+++ lab2/Lab2/src/test/ParserTests.java
@@ -56,7 +56,12 @@ public class ParserTests {
 	
 	@Test
 	public void test4() {
-		runtest("module Test { import TensorFlow; int a; public boolean foo(int b, int c){ if (a > b) return;} }");
+		runtest("module Test { import TensorFlow; int a; public boolean foo(int b, int c){ if(a > 20){return b[23]==c;} } }");
+	}
+	
+	@Test
+	public void test5() {
+		runtest("module Test { import TensorFlow; int a; public boolean foo(int b, int c){a; b = 3;  hello = dunno(c, d, 2018%99) + (25 / c*d); not_happy = \"NOT HAPPY\"; } }");
 	}
 	
 }
